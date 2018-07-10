9df7aaec7868ce4691cd436d9f2aaad3955b4d48
Rose <rose1264@gmail.com>
2018-07-09 22:17:27 -0400

    could't proceed without database

---
+++ Rakefile
@@ -0,0 +1,9 @@
+require 'active_record'
+
+task :environment do
+  require_relative './config/environment'
+end
+
+task :console => :environment do
+  Pry.start
+end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,11 +1,11 @@
-class Gallery
+class Gallery < ActiveRecord::Base
 
   attr_reader :name, :city
 
-def initialize(name, city)
-  @name = name
-  @city = city
-end
+	def initialize(name, city)
+	  @name = name
+	  @city = city
+	end
 
 
 end
